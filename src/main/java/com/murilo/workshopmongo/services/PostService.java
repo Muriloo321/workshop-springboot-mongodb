6ec1e44e6d8ead3362d984b70da7d39f9d2f8b9d
Murilo <gamermurilo0@gmail.com>
2024-08-06 22:06:31 -0300

    PostResource, PostService classes

---
+++ src/main/java/com/murilo/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.murilo.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.murilo.workshopmongo.domain.Post;
+import com.murilo.workshopmongo.repository.PostRepository;
+import com.murilo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service //Lembrar: Esta classe serve para encapsular a lógica do serviçp. No caso, os métodos usados para controlar os dados.
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
