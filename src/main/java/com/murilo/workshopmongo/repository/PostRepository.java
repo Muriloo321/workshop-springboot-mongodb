81c4ef5175e35b1f51f8d6e3ca07d5c4bc896113
Murilo <gamermurilo0@gmail.com>
2024-08-07 22:35:05 -0300

    specified queries. Last commit

--- src/main/java/com/murilo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/murilo/workshopmongo/repository/PostRepository.java
@@ -1,14 +1,22 @@
 package com.murilo.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.murilo.workshopmongo.domain.Post;
 
 @Repository //Usada para indicar que essa classe será responsável pelo acesso e manipulação de dados do banco de dados
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
